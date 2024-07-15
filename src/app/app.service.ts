57fb4196c3643b6e14877eaf8be1ff205d916a4d
Pravin Kittappa <pravink916@gmail.com>
2024-07-15 20:29:22 +0200

    feat: use the service

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,12 +1,13 @@
 import { computed, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, tap } from 'rxjs';
+import { map, Observable, tap } from 'rxjs';
 
+export type Response = {data:string}
 @Injectable()
 export class AppService {
   constructor(private http: HttpClient) { }
 
   getPravin = (): Observable<string>=>{
-    return this.http.get<string>('https://my-app-be-9qgo.onrender.com/pravin').pipe(tap((x)=>{console.log('XXX',x)}))
+    return this.http.get<Response>('https://my-app-be-9qgo.onrender.com/pravin').pipe(map((x:Response) => x.data))
   };
 }
