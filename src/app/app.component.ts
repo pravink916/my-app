c11b2a3821cc03d7db9ea5cf6e38c25b6a2ae81f
Pravin Kittappa <pravink916@gmail.com>
2024-08-24 13:09:04 +0200

    feat(landing-page): header added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { AppService } from './app.service';
 import { Observable } from 'rxjs';
 import { toSignal } from '@angular/core/rxjs-interop';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   providers: [AppService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
