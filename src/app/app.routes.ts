76affec277a728031fc8ff0a14411170bdbb8d60
Pravin Kittappa <pravink916@gmail.com>
2024-08-25 18:09:17 +0200

    feat(portfolio): Create portfolio component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { ResumeComponent } from './resume/resume.component';
 import { LandingComponent } from './landing/landing.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 export const routes: Routes = [
     {path: 'resume', component: ResumeComponent, title: 'Resume'},
     {path: 'landing', component: LandingComponent, title: 'Landing'},
+    {path: 'portfolio', component: PortfolioComponent, title: 'Portfolio'},
     {path: '', redirectTo: '/landing', pathMatch: 'full'}
 ];
