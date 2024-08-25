68ffbb674d7feee317a21db646463e59ad6353e8
Pravin Kittappa <pravink916@gmail.com>
2024-08-25 16:32:05 +0200

    feat(landing): adding landing page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { ResumeComponent } from './resume/resume.component';
+import { LandingComponent } from './landing/landing.component';
 
 export const routes: Routes = [
-    {path: 'resume', component: ResumeComponent}
+    {path: 'resume', component: ResumeComponent, title: 'Resume'},
+    {path: 'landing', component: LandingComponent, title: 'Landing'},
+    {path: '', redirectTo: '/landing', pathMatch: 'full'}
 ];
