0d4bd03c5d7bbd39a1fcc9f3a04cd8da2957c7b4
Pravin Kittappa <pravink916@gmail.com>
2024-09-26 10:03:51 +0200

    positioning sections one below another

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { IntroComponent } from '../intro/intro.component';
+import { AboutComponent } from "../about/about.component";
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [IntroComponent],
+  imports: [IntroComponent, AboutComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss',
 })
